b1b317171e65695c070e6e8710315b8e2b6d55e7
MichalZima <michal.zima01@gmail.com>
2020-06-22 16:20:03 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('miko');
+  const [age, setAge] = useState('18');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter name:</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder='napr. miko flosso'
+        onChangeText = {(val) => setName(val)}/>
+
+  <Text>name: {name}, age: {age}</Text>
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 2,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  }
 });
