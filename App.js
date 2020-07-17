4b20ab675190e2f00fa206a8f5080f6fdfe91488
MichalZima <michal.zima01@gmail.com>
2020-07-17 12:09:46 +0200

    making todo app

--- App.js
+++ App.js
@@ -1,48 +1,32 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import Header from "./components/header";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'renko', id: '1' },
-    { name: 'miko', id: '2' },
-    { name: 'adamko', id: '3' },
-    { name: 'marko', id: '4' },
-    { name: 'semi', id: '5' },
-    { name: 'siky', id: '6' },
-    { name: 'ondek', id: '7' },
+  const [todos, setTodos] = useState([
+    { text: 'programovanie', key: '1' },
+    { text: 'obchod', key: '2' },
+    { text: 'obed', key: '3' },
   ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
-
-
-
 
   return (
     <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
 
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data = {people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem = {({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          
+          />
+        </View>
 
-    {/* <ScrollView>
-      {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-      ))}
-    </ScrollView> */}
+      </View>
+
+
 
     </View>
   );
@@ -55,16 +39,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    //alignItems: 'center',
-   // justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 25,
-    marginHorizontal: 20,
-  }
+  content: {
+    padding: 40, 
+  },
+  list: {
+    paddingTop: 20,
+    marginTop: 30,
+  },
 });
