c00a3e7a1fd95cc3236e77c2f68321490d62a10c
MichalZima <michal.zima01@gmail.com>
2020-07-18 14:16:45 +0200

    dismiss keyboard function

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Keyboard, Alert, View, TextInput, FlatList, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import Header from "./components/header";
 import TodoItem from "./components/todoItems";
 import AddTodo from './components/addTodo'
@@ -18,35 +18,41 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    })
+    if (text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    }else {
+      Alert.alert ('OOPS', 'SLABE TODO', [
+        {text: 'Understood', onPress: () => console.log ('miko je sefino')}
+      ])
+    }
   }
 
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem = {({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem = {({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
-
       </View>
-
-
-
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
