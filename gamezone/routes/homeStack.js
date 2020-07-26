9fad5003cf7b7124741f9e8c220951209d8dda45
MichalZima <michal.zima01@gmail.com>
2020-07-26 18:42:24 +0200

    home stack navigator

---
+++ gamezone/routes/homeStack.js
@@ -0,0 +1,29 @@
+import { createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Home from '../screens/home';
+import ReviewDetails from '../screens/reviewDetails';
+
+const screens = {
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+        }
+    },
+    ReviewDetails: {
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+        }
+    }
+}
+
+
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#ddd', height: 60}
+    }
+});
+
+export default createAppContainer(HomeStack);
